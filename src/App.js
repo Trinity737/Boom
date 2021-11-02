346638739a8d4b1d4c673a19857aa51bf9d2e0fd
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-11-02 00:48:36 -0400

    updated Weather css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
         <footer>
-          <h1>Weather App</h1>
           This project was coded by Cheavon Pottinger and is{" "}
           <a href="https://github.com/Trinity737/Boom" Target="_blank">
             open-sourced on Github
