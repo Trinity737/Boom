416660b5fcca41a4ff5490edbffa6b78c3d06861
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-11-01 21:46:21 -0400

    added repo Url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      This page was coded by Cheavon Pottinger and is{" "}
+      <a href="https://github.com/Trinity737/Boom">open-sourced on Github</a>
+    </div>
+  );
 }
