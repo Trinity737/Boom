3a530c869e0400d6522211a1ad2a6e49b4ee1111
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-11-01 22:17:36 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      This page was coded by Cheavon Pottinger and is{" "}
-      <a href="https://github.com/Trinity737/Boom">open-sourced on Github</a>
+      <div className="container">
+        <footer>
+          <h1>Weather App</h1>
+          This project was coded by Cheavon Pottinger and is{" "}
+          <a href="https://github.com/Trinity737/Boom" Target="_blank">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
