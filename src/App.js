3b480f2e3dfb9fba76e6b45f145a73cf1364c54c
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-11-03 21:30:34 -0400

    reinstalled react added api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by Cheavon Pottinger and is{" "}
           <a href="https://github.com/Trinity737/Boom" Target="_blank">
