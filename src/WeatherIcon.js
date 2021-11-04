4b01dd5587e6b06e399777d9e120b685ef5ac772
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-11-04 02:05:56 -0400

    added temperature components

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,12 +1,34 @@
 import React from "react";
+
 import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
+  const codeMapping = {
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "SCATTERED_CLOUDS_DAY",
+    "03n": "SCATTERED_CLOUDS_NIGHT",
+    "04d": "BROKEN_CLOUDS_DAY",
+    "04n": "BROKEN_CLOUDS_NIGHT",
+    "09d": "SHOWER_RAIN_DAY",
+    "09n": "SHOWER_RAIN_NIGHT",
+    "010d": "RAIN_DAY",
+    "010n": "RAIN_NIGHT",
+    "011d": "THUNDERSTORM_DAY",
+    "011n": "THUNDERSTORM_NIGHT",
+    "013d": "SNOW_DAY",
+    "013n": "SNOW_NIGHT",
+    "050d": "MIST_DAY",
+    "050n": "MIST_NIGHT",
+  };
+
   return (
     <ReactAnimatedWeather
-      icon="CLEAR_DAY"
-      color="red"
-      size={64}
+      icon={codeMapping[props.code]}
+      color="#1e1e1e"
+      size={52}
       animate={true}
     />
   );
